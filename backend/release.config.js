0bef9c4af6e5ab424668088297469ef7a5fb1102
dai.trinhduc <dai.trinhduc@ncc.asia>
2022-07-22 17:09:12 +0700

    init

---
+++ backend/release.config.js
@@ -0,0 +1,19 @@
+const releaseConfig = {
+  release: {
+    branch: ["main"],
+  },
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github",
+    [
+      "@semantic-release/changelog",
+      {
+        "changelogFile": "docs/CHANGELOG.md"
+      }
+    ],
+  ],
+  extends: "semantic-release-monorepo",
+};
+
+module.exports = releaseConfig;
